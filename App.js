5c4ee1858ed992e46d4c6c2b6725e4b2bf2aba45
Sam Morris <s.morris12151@gmail.com>
2023-05-04 11:16:11 -0400

    add master book constructor and component constructors

--- App.js
+++ App.js
@@ -1,101 +1,95 @@
+/* eslint-disable prefer-destructuring */
 /* eslint-disable func-names */
 /* eslint-disable no-empty */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-unused-vars */
 const myLibrary = [];
-const libraryContainer = document.getElementById("bookContainer");
-const bookContainer = document.getElementById("bookContainer");
-function Book(title, author, length, readStatus) {
+const libraryContainer = document.getElementById("libraryContainer");
+
+function Book(title, author, length, status, rating) {
   this.title = title;
   this.author = author;
   this.length = length;
-  this.readStatus = readStatus;
+  this.staus = status;
+  this.rating = rating;
 }
 Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.length}, ${this.readStatus}`;
+  return `${this.title} by ${this.author}, ${this.length}, ${this.status}`;
 };
-
-const theHobbit = new Book("The Hobbit", "JRR Tolkein", "295 pages", "No");
-const theHobbit2 = new Book("The Hobbit2", "JRR Tolkein2", "295 pages2", "No2");
-const theHobbit3 = new Book("The Hobbit3", "JRR Tolkein3", "295 pages3", "No3");
-const theHobbit4 = new Book("The Hobbit4", "JRR Tolkein4", "295 pages4", "No4");
-const theHobbit5 = new Book("The Hobbit5", "JRR Tolkein5", "295 pages5", "No5");
-const theHobbit6 = new Book("The Hobbit6", "JRR Tolkein6", "295 pages6", "No6");
 
 function addBookToLibrary(book) {
   if (!myLibrary.includes(book)) {
     myLibrary.push(book);
   }
 }
-addBookToLibrary(theHobbit);
-addBookToLibrary(theHobbit2);
-addBookToLibrary(theHobbit3);
-addBookToLibrary(theHobbit4);
-addBookToLibrary(theHobbit5);
-addBookToLibrary(theHobbit6);
 
-function buildTitle(libraryBook, i) {
+function buildTitle() {
+  const title = document.createElement("div");
+  title.textContent = document.getElementById("title").value;
+  return title.textContent;
+}
+
+function buildAuthor() {
+  const author = document.createElement("div");
+  author.textContent = document.getElementById("author").value;
+  return author.textContent;
+}
+function buildPageCount() {
+  const pageCount = document.createElement("div");
+  pageCount.textContent = document.getElementById("pageCount").value;
+  return pageCount.textContent;
+}
+function buildStatus() {
+  const status = document.createElement("div");
+  status.textContent = document.getElementById("status").value;
+  return status.textContent;
+}
+
+function buildRating() {
+  const rating = document.createElement("div");
+  rating.textContent = document.getElementById("rating").value;
+  return rating.textContent;
+}
+
+function printCard() {
+  const card = document.createElement("div");
+  card.id = "card";
   const title = document.createElement("div");
   title.id = "title";
-  title.textContent = `Title: ${myLibrary[i].title}`;
-  libraryBook.appendChild(title);
-}
-function buildAuthor(libraryBook, i) {
+  title.textContent = buildTitle();
+  card.appendChild(title);
+
   const author = document.createElement("div");
   author.id = "author";
-  author.textContent = `Written By: ${myLibrary[i].author}`;
-  libraryBook.appendChild(author);
-}
-function buildLength(libraryBook, i) {
-  const length = document.createElement("div");
-  length.id = "length";
-  length.textContent = `Length: ${myLibrary[i].length}`;
-  libraryBook.appendChild(length);
-}
-function buildReadStatus(libraryBook, i) {
-  const readStatus = document.createElement("div");
-  readStatus.id = "readStatus";
-  readStatus.textContent = `Read? ${myLibrary[i].readStatus}`;
-  libraryBook.appendChild(readStatus);
-}
+  author.textContent = buildAuthor();
+  card.appendChild(author);
 
-function buildNewBookButton() {
-  const newBookButton = document.createElement("img");
-  newBookButton.id = "newBookButton";
-  newBookButton.setAttribute("src", "img/noun-add-book-111365.svg");
-  newBookButton.setAttribute("alt", "add book");
-  libraryContainer.appendChild(newBookButton);
-  newBookButton.addEventListener("click", () => {
-    const bookSubmission = document.getElementById("bookSubmission");
-    bookSubmission.style.display = "grid";
-    bookContainer.append(bookSubmission);
-    newBookButton.style.display = "none";
-  });
-}
-function noSubmit(event) {
-  event.preventDefault();
-}
+  const pageCount = document.createElement("div");
+  pageCount.id = "pageCount";
+  pageCount.textContent = buildPageCount();
+  card.appendChild(pageCount);
 
+  const status = document.createElement("div");
+  status.id = "status";
+  status.textContent = buildStatus();
+  card.appendChild(status);
+
+  const rating = document.createElement("div");
+  rating.id = "rating";
+  rating.textContent = buildRating();
+  card.appendChild(rating);
+
+  libraryContainer.appendChild(card);
+}
 function buildBook() {
-  const addBook = document.getElementById("addBook");
-  addBook.addEventListener("hover", () => {
-    addBook.style.color = "coral";
-  });
+  const newBook = new Book(
+    buildTitle(),
+    buildAuthor(),
+    buildPageCount(),
+    buildStatus(),
+    buildRating()
+  );
+  addBookToLibrary(newBook);
+  printCard();
+  // alert(myLibrary[myLibrary.length - 1].author);
 }
-
-function buildLibrary() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    const libraryBook = document.createElement("div");
-    libraryBook.id = "libraryBook";
-    libraryContainer.appendChild(libraryBook);
-    buildTitle(libraryBook, i);
-    buildAuthor(libraryBook, i);
-    buildLength(libraryBook, i);
-    buildReadStatus(libraryBook, i);
-    if (i === myLibrary.length - 1) {
-      buildNewBookButton();
-    }
-  }
-}
-
-buildLibrary(myLibrary);
--- README.md
+++ README.md
@@ -5,3 +5,32 @@ image credits:
 
     book svg from thenounproject.com:
         Add Book by Creative Stall from <a href="https://thenounproject.com/browse/icons/term/add-book/" target="_blank" title="Add Book Icons">Noun Project</a>
+
+Pseudo:
+
+//input form gives data to individual constructors. On submit, form dissapears and is replaced by finished card. New book button repopulates to the right of the new card.
+//individual constructors output data into master book constructor
+//that constructor calls each constructor function, and sets each output to a new const variable - title, author, lenght, status, rating
+//these constants are then input into the Book function to build a new Book Object
+//that new book object is then added to the myLibrary array
+//a card print function then takes the info from the latest myLibrary array entry (.length-1) and builds a card for that entry
+
+//function masterConstructor(title, author, length, status, rating){
+//const title = buildTitle()
+// const author = buildAuthor()
+// const length = buildLength()
+// const status = buildStatus()
+// const rating = buildRating()
+// const newBook = new Book(title, author, length, status, rating)
+// addBookToLibrary(newBook)
+// printCard()
+//}
+
+// when clicking the addBookButton:
+//function displayForm(){
+// addBookButton.style.display = none
+//const inputForm = document.createElementfromID('booksubmission')
+//bookContainer.amendChild(inputForm)
+//inputForm.style.display=grid
+
+//} that should make the button go away, and the form appear in its place
